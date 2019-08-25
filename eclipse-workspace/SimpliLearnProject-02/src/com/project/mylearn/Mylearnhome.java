b8296adf85fe90192424ce5104349b5f12f7e2a3
Abhinav Kumar <abhinavkumarvikram@gmail.com>
2019-08-24 22:25:02 -0400

    My first commit

---
+++ eclipse-workspace/SimpliLearnProject-02/src/com/project/mylearn/Mylearnhome.java
@@ -0,0 +1,11 @@
+package com.project.mylearn;
+
+public class Mylearnhome {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub ..This project is created to test Simplilern learning.
+
+		System.out.println("This for learning purpose");
+	}
+
+}
